02487ac309330b8bcfc78dbc1434042acfe94c0b
João  Víctor Antunes <67442552+DevTrzJbr@users.noreply.github.com>
2025-08-27 15:32:39 -0300

    fix: atualização endpoint rotaCsv para rota

--- backend-node/src/routes/landing.routes.js
+++ backend-node/src/routes/landing.routes.js
@@ -9,7 +9,7 @@ router.get('/', (_req, res) => {
         version: '1.0.0',
         endpoints: {
             healthCheck: { method: 'GET', url: '/ping', description: 'Verifica se o servidor está online' },
-            rotaCsv: { method: 'GET', url: '/api/rota/:nomeDaRota', description: 'Retorna pontos de uma rota CSV' },
+            rota: { method: 'GET', url: '/api/rota/:nomeDaRota', description: 'Retorna pontos de uma rota CSV' },
             veiculos: { method: 'GET', url: '/api/veiculos', description: 'Lista veículos cadastrados' }
         },
         example: {
