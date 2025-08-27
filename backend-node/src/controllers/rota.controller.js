65ec7a70c19f01a950f4c6e1a66c00b1a1d7fc32
João  Víctor Antunes <67442552+DevTrzJbr@users.noreply.github.com>
2025-08-27 15:31:00 -0300

    feat: Exposição de rota para padrão GeoJSON na API

--- backend-node/src/controllers/rota.controller.js
+++ backend-node/src/controllers/rota.controller.js
@@ -1,5 +1,6 @@
 // src/controllers/rota.controller.js
-const { getRotaGeoJson } = require('../services/rota.service');
+// const { getRotaGeoJson } = require('../services/rota.service');
+const { getRotaGeoJson } = require('../services/geojson.service');
 
 async function getRota(req, res, next) {
   try {
---
+++ backend-node/src/services/geojson.service.js
@@ -0,0 +1,29 @@
+// src/services/geojson.service.js
+const fs = require('fs').promises;
+const path = require('path');
+
+async function getRotaGeoJson(rotaId) {
+  // Exemplo: projeto/temp/rota_normal.csv
+  const filePath = path.join(__dirname, '../../..', 'temp', `${rotaId}.json`);
+
+  console.log('Chamou!!!');
+  console.log(filePath);
+  
+  
+  try {
+      const data = await fs.readFile(filePath, 'utf-8'); // lê o conteúdo como string
+      const json = JSON.parse(data); // converte em objeto JS
+      
+      console.log(json);
+      return json;
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo ${filePath}:`, err);
+    throw err; // propaga o erro para quem chamou a função
+  }
+
+  return filePath;
+}
+
+module.exports = {
+  getRotaGeoJson,
+};
